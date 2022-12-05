e74616a6abdb39ff41a0a497fa3c13a8974c0679
Dewa Mahendra <31196162+mahendraputra21@users.noreply.github.com>
2022-12-05 17:50:12 +0800

    Add files via upload

---
+++ BSAProductCRUD-API/Models/ProductDBContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BSAProductCRUD_API.Models
+{
+    public partial class ProductDBContext : DbContext
+    {
+        public ProductDBContext(DbContextOptions<ProductDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        optionsBuilder.UseSqlServer("Server=.;Database=ProductDB;Integrated Security=True");
+        //    }
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Description)
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
